b1850a528a3d95cfdc868b0e1dbed91949972cf0
Kapil Reddy <67938512+Kapilreddy2309@users.noreply.github.com>
2021-11-12 05:40:39 +0530

    Added new notebook

---
+++ notebooks/Test.py
@@ -0,0 +1,89 @@
+# Databricks notebook source
+print("HI")
+
+# COMMAND ----------
+
+blob_account_name = "azureopendatastorage"
+blob_container_name = "citydatacontainer"
+blob_relative_path = "Safety/Release/city=Seattle"
+blob_sas_token = r"?st=2019-02-26T02%3A34%3A32Z&se=2119-02-27T02%3A34%3A00Z&sp=rl&sv=2018-03-28&sr=c&sig=XlJVWA7fMXCSxCKqJm8psMOh0W4h7cSYO28coRqF2fs%3D"
+
+# COMMAND ----------
+
+wasbs_path = 'wasbs://%s@%s.blob.core.windows.net/%s' % (blob_container_name, blob_account_name, blob_relative_path)
+spark.conf.set('fs.azure.sas.%s.%s.blob.core.windows.net' % (blob_container_name, blob_account_name), blob_sas_token)
+print('Remote blob path: ' + wasbs_path)
+
+# COMMAND ----------
+
+df = spark.read.parquet(wasbs_path)
+print('Register the DataFrame as a SQL temporary view: source')
+df.createOrReplaceTempView('source')
+
+# COMMAND ----------
+
+print('Displaying top 10 rows: ')
+display(spark.sql('SELECT * FROM source LIMIT 10'))
+
+# COMMAND ----------
+
+# Pip install packages
+import os, sys
+
+!{sys.executable} -m pip install azure-storage-blob
+!{sys.executable} -m pip install pyarrow
+!{sys.executable} -m pip install pandas
+
+# COMMAND ----------
+
+# Azure storage access info
+azure_storage_account_name = "azureopendatastorage"
+azure_storage_sas_token = r""
+container_name = "citydatacontainer"
+folder_name = "Safety/Release/city=Seattle"
+
+# COMMAND ----------
+
+from azure.storage.blob import *
+
+if azure_storage_account_name is None or azure_storage_sas_token is None:
+    raise Exception(
+        "Provide your specific name and key for your Azure Storage account--see the Prerequisites section earlier.")
+
+print('Looking for the first parquet under the folder ' +
+      folder_name + ' in container "' + container_name + '"...')
+container_url = f"https://{azure_storage_account_name}.blob.core.windows.net/"
+blob_service_client = BlobServiceClient(
+    container_url, azure_storage_sas_token if azure_storage_sas_token else None)
+
+container_client = blob_service_client.get_container_client(container_name)
+blobs = container_client.list_blobs(folder_name)
+sorted_blobs = sorted(list(blobs), key=lambda e: e.name, reverse=True)
+targetBlobName = ''
+for blob in sorted_blobs:
+    if blob.name.startswith(folder_name) and blob.name.endswith('.parquet'):
+        targetBlobName = blob.name
+        break
+
+print('Target blob to download: ' + targetBlobName)
+_, filename = os.path.split(targetBlobName)
+blob_client = container_client.get_blob_client(targetBlobName)
+with open(filename, 'wb') as local_file:
+    blob_client.download_blob().download_to_stream(local_file)
+
+# COMMAND ----------
+
+# Read the parquet file into Pandas data frame
+import pandas as pd
+
+print('Reading the parquet file into Pandas data frame')
+df = pd.read_parquet(filename)
+
+# COMMAND ----------
+
+# you can add your filter at below
+print('Loaded as a Pandas data frame: ')
+df
+
+# COMMAND ----------
+
